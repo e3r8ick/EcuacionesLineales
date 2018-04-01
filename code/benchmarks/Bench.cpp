2b6c34a8b27e795c0713ac43a0da5acafacba739
e3r8ick <eguicoro2@gmail.com>
2018-04-01 16:53:15 -0600

    Clase para Benchmarks

---
+++ code/benchmarks/Bench.cpp
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2018
+ * Área Académica de Ingeniería en Computadoras, ITCR, Costa Rica
+ *
+ * This file is part of the numerical analysis lecture CE3102 at TEC
+ *
+ * @Author: ErickCordero
+ * @Date  : 03.03.2018
+ */
+
+#include <cmath>
+#include <limits>
+#include <functional>
+#include <iostream>
+#include <algorithm>
+
+#include "Exception.hpp"
+#include "Matrix.hpp"
+#include "Pivot.hpp"
+
+#ifndef ANPI_BENCH_HPP
+#define ANPI_BENCH_HPP
+
+namespace anpi
+{
+
+
+} //anpi
+
+#endif
