970a7eb230c8e6001efeda82c6ebfb4341eba394
vic6595 <vic9565@hotmail.com>
2018-04-04 17:38:13 -0600

    added solveQR

--- code/include/QRdecomp.hpp
+++ code/include/QRdecomp.hpp
@@ -44,6 +44,7 @@ namespace anpi
 			if (scale==0.0){
 				singular=true;
 				c[k]=d[k]=0.0;	
+				throw anpi::Exception("Intentando resolver sistema singular");
 			}else {
 				for (int i=k;i<n;i++){
 					R(i,k) = R(i,k)/ scale;
@@ -73,6 +74,7 @@ namespace anpi
 		d[n-1]=R(n-1,n-1);
 		if (d[n-1]==0.0){
 			singular=true;
+			throw anpi::Exception("Intentando resolver sistema singular");
 		}
 		for (int i=0;i<n;i++){
 			for (int j=0;j<n;j++){
@@ -104,6 +106,71 @@ namespace anpi
 		//std::cout<<"singular: "<< singular<< std::endl;
 		
 	}
+
+
+	template<typename T>
+	bool solveQR(const anpi::Matrix<T>& A,
+		std::vector<T>& x,
+		const std::vector<T>& b){
+ 	 	std::cout<<" entra a solveQR "<<std::endl;
+
+ 	 	anpi::Matrix<float> Q=anpi::Matrix<float>(A.rows(),A.cols(),0.0);
+  		anpi::Matrix<float> R=anpi::Matrix<float>(A.rows(),A.cols(),0.0);
+		anpi::qr(A,Q,R);
+		
+
+		std::cout << "A" << std::endl;
+		printM(A);
+		std::cout << "Q" << std::endl;
+		printM(Q);
+		std::cout << "R" << std::endl;
+		printM(R);
+
+
+		qtMult(b,x,Q);
+		rSolve(b,x,R);
+
+	}
+
+
+	template<typename T>
+	void qtMult(const std::vector<T>& b,std::vector<T>& x,anpi::Matrix<T>& Q){
+		std::cout<<" entra a qtMult "<<std::endl;
+		T sum;
+		int n=Q.rows();
+		for (int i=0;i<n;i++){
+			sum=0.0;
+			for (int j=0;j<n;j++){
+				sum= sum+ Q(i,j)*b[j];
+				x[i]=sum;
+				std::cout << "x[i]" << x[i] << std::endl;
+			}
+		}
+	}
+
+	template<typename T>
+	void rSolve(const std::vector<T>& b,std::vector<T>& x,anpi::Matrix<T>& R ){
+		std::cout<<" entra a rSolve "<<std::endl;
+		T sum;
+		int n=R.rows();
+		for (int i=n-1;i>=0;i--){
+			sum=b[i];
+			for(int j=i+1;j<n;j++){
+				sum = sum- R(i,j)*x[j];
+				std::cout << "sum: " << sum << std::endl;
+			}
+			std::cout << "R(i,i)" << R(i,i) << std::endl;
+			std::cout << "Xi: ";
+			std::cout << x[i] << std::endl;
+			x[i]=sum/R(i,i);
+			
+		}
+	}
+
+
+
+
+
 
 	
 }
