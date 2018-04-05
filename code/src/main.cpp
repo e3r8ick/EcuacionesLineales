c7cd0ff177d8899b5045b4825a166414cb3121da
Cristofer Villegas <zeldardar@hotmail.com>
2018-04-04 21:16:19 -0600

    Main con pruebas

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -16,55 +16,72 @@
 #include "LUCrout.hpp"
 #include "Pivot.hpp"
 #include "QRdecomp.hpp"
+#include "Invert.hpp"
+int main()
+{
 
-int main() {
-
-    anpi::Matrix<float> A = { {-1,-2,1},
-                          { 2, 0,1},
-                          {-1,-1,0}
-                          };
-  anpi::InitializationType init=anpi::InitializationType::DoNotInitialize;
-  anpi::Matrix<float> Q=anpi::Matrix<float>(A.rows(),A.cols(),init);
-  anpi::Matrix<float> R=anpi::Matrix<float>(A.rows(),A.cols(),init);
-  anpi::qr(A,Q,R);
-
-  bool test =false;
-
-  anpi::Matrix<float> Qt=anpi::Matrix<float>(A.rows(),A.cols(),init);
-  anpi::Matrix<float> I=anpi::Matrix<float>(A.rows(),A.cols(),init);
-  for(int i=0;i<A.rows();i++){
-    for(int j=0;j<A.cols();j++){
-      Qt(j,i)=Q(i,j);
-    }
-  }
-
-  for(int i=0;i<A.rows();i++){
-    for(int j=0;j<A.cols();j++){
-      I(i,j)=0;
-      I(i,i)=1;
-    }
-  }
-
-  anpi::Matrix<float> tmp=anpi::Matrix<float>(A.rows(),A.cols(),init);
-  tmp=Qt*R;
-
-
-  test= (I==tmp);
-
-  std::cout << "A" << std::endl;
-  printM(A);
-  std::cout << "Q" << std::endl;
+  anpi::Matrix<float> A = {{-1, -2, 1},
+                           {2, 0, 1},
+                           {-1, -1, 0}};
+  anpi::Matrix<float> LU,L,U,LUC,LC,UC,Q,R,Qt, Ai;
+  std::vector<float> x;
+  std::vector<float> b = {1,0,-1};
+  std::vector<size_t> permut, permutC;
+  std::cout << "Doolittle" << std::endl;
+  anpi::luDoolittle(A,LU,permut);
+  std::cout << "LU: " << std::endl;
+  printM(LU);
+  anpi::unpackDoolittle(LU,L,U);
+  std::cout << "L: " << std::endl;
+  printM(L);
+  std::cout << "U: " << std::endl;
+  printM(U);
+  std::cout << "L*U" <<std::endl;
+  printM(L*U);
+  std::cout << "Crout " << std::endl;
+  anpi::luCrout(A,LUC, permut);
+  std::cout << "LU: " << std::endl;
+  printM(LUC);
+  anpi::unpackCrout(LUC,LC,UC);
+  std::cout << "L: " << std::endl;
+  printM(LC);
+  std::cout << "U: " << std::endl;
+  printM(UC);
+  std::cout << "L*U" <<std::endl;
+  printM(LC*UC);
+  std::cout << "Qr decomp" << std::endl;
+  anpi::qr(A, Q, R);
+  std::cout << "Q: " << std::endl;
   printM(Q);
-  std::cout << "R" << std::endl;
+  std::cout << "R: " << std::endl;
   printM(R);
-  std::cout << "I" << std::endl;
-  printM(I);
-  std::cout << "QT" << std::endl;
-  printM(Qt);
-  std::cout << "tmp" << std::endl;
-  printM(tmp);
-
-  std::cout<<"Q*Qt ? =I : "<<test<<std::endl;
-  return 1;
-}
-  
+  Qt = Q;
+  for (int i = 0; i < (int) Q.rows(); ++i){
+    for (int j = 0 ; j < (int) Q.rows(); ++j){
+      Qt[j][i] = Q[i][j];
+    }
+  }
+  std::cout << "Qt *R " <<std::endl;
+  printM(Qt*R);
+  std::cout << "Inversion " << std::endl;
+  anpi::invert(A, Ai);
+  std::cout << "Ai : " << std::endl;
+  printM(Ai);
+  std::cout << "Identity" << std::endl;
+  printM(A*Ai);
+  std::cout << "Solve B: LU" << std::endl;
+  anpi::solveLU(A,x,b);
+  std::cout << "x: " << std::endl;
+  for (int i = 0; i < (int) x.size(); ++i){
+    std::cout << x[i] << "\t";
+  }
+  std::cout << std::endl;
+  x[0] = x[1] = x[2] = 0;
+  std::cout << "Solve B: QR" << std::endl;
+  anpi::solveQR(A, x, b);
+  std::cout << "x: " << std::endl;
+  for (int i = 0; i < (int) x.size(); ++i){
+    std::cout << x[i] << "\t";
+  }
+  std::cout << std::endl;
+  }
